9fb709689af7d29c85834bc28f6542e89350ebc4
hanchenxing1 <91150699+hanchenxing1@users.noreply.github.com>
2023-07-12 22:22:44 +0800

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -61,9 +61,6 @@ const Footer = () => {
 
       <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
         <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
-          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
-            NFTKastle, Inc. All Rights Reserved
-          </p>
           <div className="flex flex-row sm:mt-4">
             {[
               images.instagram,
